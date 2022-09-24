d7f5ff6e41f91d83843afd1ec9d261920725abe1
mayankgxp <mayankg303@Gmail.com>
2022-09-24 12:23:35 +0530

    Third COmmit

--- Program.cs
+++ Program.cs
@@ -20,8 +20,7 @@ namespace WebApplication2
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                    
+                    webBuilder.UseStartup<Startup>();                    
                 });
     }
 }
